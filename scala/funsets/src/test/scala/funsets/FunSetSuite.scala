1587debd70a59e9aec179672b0f4eaf6dab403ac
Zach Strudler <zstrudler@tripadvisor.com>
2013-04-13 13:54:22 -0400

    assignment 2

---
+++ scala/funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -0,0 +1,175 @@
+package funsets
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+/**
+ * This class is a test suite for the methods in object FunSets. To run
+ * the test suite, you can either:
+ *  - run the "test" command in the SBT console
+ *  - right-click the file in eclipse and chose "Run As" - "JUnit Test"
+ */
+@RunWith(classOf[JUnitRunner])
+class FunSetSuite extends FunSuite {
+
+
+  /**
+   * Link to the scaladoc - very clear and detailed tutorial of FunSuite
+   *
+   * http://doc.scalatest.org/1.9.1/index.html#org.scalatest.FunSuite
+   *
+   * Operators
+   *  - test
+   *  - ignore
+   *  - pending
+   */
+
+  /**
+   * Tests are written using the "test" operator and the "assert" method.
+   */
+  test("string take") {
+    val message = "hello, world"
+    assert(message.take(5) == "hello")
+  }
+
+  /**
+   * For ScalaTest tests, there exists a special equality operator "===" that
+   * can be used inside "assert". If the assertion fails, the two values will
+   * be printed in the error message. Otherwise, when using "==", the test
+   * error message will only say "assertion failed", without showing the values.
+   *
+   * Try it out! Change the values so that the assertion fails, and look at the
+   * error message.
+   */
+  test("adding ints") {
+    assert(1 + 2 === 3)
+  }
+
+  
+  import FunSets._
+
+  test("contains is implemented") {
+    assert(contains(x => true, 100))
+  }
+  
+  /**
+   * When writing tests, one would often like to re-use certain values for multiple
+   * tests. For instance, we would like to create an Int-set and have multiple test
+   * about it.
+   * 
+   * Instead of copy-pasting the code for creating the set into every test, we can
+   * store it in the test class using a val:
+   * 
+   *   val s1 = singletonSet(1)
+   * 
+   * However, what happens if the method "singletonSet" has a bug and crashes? Then
+   * the test methods are not even executed, because creating an instance of the
+   * test class fails!
+   * 
+   * Therefore, we put the shared values into a separate trait (traits are like
+   * abstract classes), and create an instance inside each test method.
+   * 
+   */
+
+  trait TestSets {
+    val s1 = singletonSet(1)
+    val s2 = singletonSet(2)
+    val s3 = singletonSet(3)
+
+    val u1 = union(s1, s2)
+    val u2 = union(s2, s3)
+
+    val sAll = union(union(s1, s2), s3)
+  }
+
+  /**
+   * This test is currently disabled (by using "ignore") because the method
+   * "singletonSet" is not yet implemented and the test would fail.
+   * 
+   * Once you finish your implementation of "singletonSet", exchange the
+   * function "ignore" by "test".
+   */
+  test("singletonSet(1) contains 1") {
+    
+    /**
+     * We create a new instance of the "TestSets" trait, this gives us access
+     * to the values "s1" to "s3". 
+     */
+    new TestSets {
+      /**
+       * The string argument of "assert" is a message that is printed in case
+       * the test fails. This helps identifying which assertion failed.
+       */
+      assert(contains(s1, 1), "Singleton")
+    }
+  }
+
+  test("union of 1 and 2") {
+    new TestSets {
+      assert( contains(u1, 1), "Should have 1")
+      assert( contains(u1, 2), "Should have 2")
+      assert(!contains(u1, 3), "Shouldn't have 3")
+    }
+  }
+
+  test("intersection of set(1,2)") {
+    new TestSets {
+      val s = intersect(u1, u2)
+
+      assert(!contains(s, 1), "Shouldn't have 1")
+      assert( contains(s, 2), "Should have 2")
+      assert(!contains(s, 3), "Shouldn't have 3")
+    }
+  }
+
+  test("diff of set(1,2) and set(2,3)") {
+    new TestSets {
+      val s = diff(u1, u2)
+
+      assert( contains(s, 1), "Should have 1")
+      assert(!contains(s, 2), "Shouldn't have 2")
+      assert(!contains(s, 3), "Shouldn't have 3")
+    }
+  }
+
+  test("filter of set(1,2,3) for odd numbers") {
+    new TestSets {
+      val s = filter(sAll, (x: Int) => x % 2 != 0)
+
+      assert( contains(s, 1), "Should have 1")
+      assert(!contains(s, 2), "Shouldn't have 2")
+      assert( contains(s, 3), "Should have 3")
+    }
+  }
+
+  test("forall of set(1,2,3)") {
+    new TestSets {
+      assert( forall(sAll, (x: Int) => x < 4), "Should all be <4")
+      assert(!forall(sAll, (x: Int) => x < 3), "Shouldn't all be <3")
+    }
+  }
+
+  test("exists of set(2,3)") {
+    new TestSets {
+      val divide25 = exists(u2, (x: Int) => (25/x) * x == 25)
+      val even = exists(u2, (x: Int) => x % 2 == 0)
+
+      assert(!divide25, "Shouldn't find a number that divides 25")
+      assert(even, "Should find an even number")
+    }
+  }
+
+  test("map of set(1,2,3) to x -> x + 10") {
+    new TestSets {
+      val s = map(sAll, (x: Int) => x + 10)
+      val originals = (x: Int) => x == 1 || x == 2 || x == 3
+
+      assert(!exists(s, originals), "Shouldn't have any of the original numbers")
+      assert(contains(s, 11), "Should have 11")
+      assert(contains(s, 12), "Should have 12")
+      assert(contains(s, 13), "Should have 13")
+    }
+  }
+}
